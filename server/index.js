3981603aa230192aed60ec6e4517453a0fa5b81d
Jeffrey Ganulin <jganulin6@gmail.com>
2023-04-12 11:11:18 -0400

    Add CRA prototype with Chakra Added scripts to start front- and backend using Concurrently Allow Mongoose errors to be output more reliably Use env variable for Session cookie secret Fixed some missing dependencies

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv').config();
 const connectDB = require('./config/db');
 const session = require('express-session');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const MongoStore = require('connect-mongo');
 
 const app = express();
@@ -11,15 +12,26 @@ const port = process.env.PORT;
 connectDB();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cors({
   credentials: true
 }));
 app.use(session({
-  secret: 'coolprojectmanagementSystem',
+  secret: process.env.SESSION_SECRET,
   resave: false,
-  saveUninitialized: false, // set to true in prod
-  store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+  saveUninitialized: (process.env.NODE_ENV === 'production'), // set to true in prod
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGO_URI,
+    mongoOptions: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      family: 4,
+      serverSelectionTimeoutMS: 2000
+    },
+  }, {
+    collectionName: 'sessions',
+    autoRemove: 'native'
+  }),
   cookie: {
     sameSite: 'lax',
     maxAge: 1000 * 60 * 60 * 24,
